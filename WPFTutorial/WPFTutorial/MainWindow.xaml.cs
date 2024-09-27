36446e6c93d97e8c925fec5a66575b9151784108
Luna Jia <luna.jia6@gmail.com>
2024-09-27 09:40:27 -0400

    Add click event to button

--- WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -16,11 +16,33 @@ namespace WPFTutorial
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool running = false;
         public MainWindow()
         {
             InitializeComponent();
 
-            tbHello.Text = "Hello World 2";
+            
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running) 
+            {
+                //stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+
+            }
+            else
+            {
+                //run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+           
+            running = !running;
+           
+
         }
     }
 }
