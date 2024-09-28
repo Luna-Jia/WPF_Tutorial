949f3655df65f7431310c12cf97a126e7efaa738
Luna Jia <luna.jia6@gmail.com>
2024-09-28 19:25:28 -0400

    Implement INotifyPropertyChanged

--- Tutorial8_dataBinding/Tutorial8_dataBinding/MainWindow.xaml.cs
+++ Tutorial8_dataBinding/Tutorial8_dataBinding/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tutorial8_dataBinding
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this; 
             InitializeComponent();
         }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value; 
+                // OnPropertyChanged();
+            }
+        }
+
     }
 }
