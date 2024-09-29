215b9f778d6bbfcc2bde4569ee273ffa19cbcb1a
Luna Jia <luna.jia6@gmail.com>
2024-09-29 10:34:16 -0400

    WPF12_ListView: Setup: Add textbox, buttons, and list

--- ListViewWPF12/ListViewWPF12/MainWindow.xaml.cs
+++ ListViewWPF12/ListViewWPF12/MainWindow.xaml.cs
@@ -19,6 +19,26 @@ namespace ListViewWPF12
         public MainWindow()
         {
             InitializeComponent();
+            //lvEntries.Items.Add("a");
+            //lvEntries.Items.Add("a1");
+            //lvEntries.Items.Add("a2");
+            //lvEntries.Items.Add("a3");
+        }
+
+        private void tnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Add(txtEntry.Text);
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
