ef148f7192433a4156b9aeb6aba46af7287a6cae
Luna Jia <luna.jia6@gmail.com>
2024-09-29 10:45:01 -0400

    WPF12_ListView: Delete selectedIndex

--- ListViewWPF12/ListViewWPF12/MainWindow.xaml.cs
+++ ListViewWPF12/ListViewWPF12/MainWindow.xaml.cs
@@ -19,26 +19,29 @@ namespace ListViewWPF12
         public MainWindow()
         {
             InitializeComponent();
-            //lvEntries.Items.Add("a");
-            //lvEntries.Items.Add("a1");
-            //lvEntries.Items.Add("a2");
+            lvEntries.Items.Add("a");
+            lvEntries.Items.Add("b");
+            lvEntries.Items.Add("c");
             //lvEntries.Items.Add("a3");
         }
 
         private void tnAdd_Click(object sender, RoutedEventArgs e)
         {
             lvEntries.Items.Add(txtEntry.Text);
+            txtEntry.Clear();   
 
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            int index = lvEntries.SelectedIndex;
+            lvEntries.Items.RemoveAt(index);
 
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
-
+            lvEntries.Items.Clear();
         }
     }
 }
