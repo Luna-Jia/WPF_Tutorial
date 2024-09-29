73d32f1608d32739d2f21cf3f0c551359a9c63ae
Luna Jia <luna.jia6@gmail.com>
2024-09-29 08:40:54 -0400

    Tutorial#10: OpenFileDialog

--- Tutorial10_OpenFileDialog/Tutorial10_OpenFileDialog/MainWindow.xaml.cs
+++ Tutorial10_OpenFileDialog/Tutorial10_OpenFileDialog/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tutorial10_OpenFileDialog
 {
@@ -19,6 +11,13 @@ namespace Tutorial10_OpenFileDialog
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog fileDiolog = new OpenFolderDialog();
+            fileDiolog.ShowDialog();
+
         }
     }
 }
