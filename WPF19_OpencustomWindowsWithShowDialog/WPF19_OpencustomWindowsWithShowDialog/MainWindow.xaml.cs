727ed90225bf611f4117a0332085b4630fdb0417
Luna Jia <luna.jia6@gmail.com>
2024-09-29 21:26:10 -0400

    WPF19: add normal/modal window

--- WPF19_OpencustomWindowsWithShowDialog/WPF19_OpencustomWindowsWithShowDialog/MainWindow.xaml.cs
+++ WPF19_OpencustomWindowsWithShowDialog/WPF19_OpencustomWindowsWithShowDialog/MainWindow.xaml.cs
@@ -1,24 +1,27 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using WPF19_OpencustomWindowsWithShowDialog.View;
 
 namespace WPF19_OpencustomWindowsWithShowDialog
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWindow normalWindow = new NormalWindow();
+            normalWindow.Show();
+
+        }
+
+        private void btnModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow();
+            modalWindow.ShowDialog();
+
         }
     }
 }
